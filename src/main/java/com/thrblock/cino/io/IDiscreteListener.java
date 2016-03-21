e4f249e5d750887d07966aad2a54a906a7c1b6fa
lizepu <lizepu@gyyx.cn>
2016-03-21 20:28:51 +0800

    chang IO

--- src/main/java/com/thrblock/cino/io/IDiscreteListener.java
+++
@@ -1,7 +0,0 @@
-package com.thrblock.cino.io;
-
-public interface IDiscreteListener {
-	public void discreteTyped(int code);
-	public void discreteActivate(int code);
-	public void discreteDeactivate(int code);
-}
---
+++ src/main/java/com/thrblock/cino/io/IKeyControlStack.java
@@ -0,0 +1,16 @@
+package com.thrblock.cino.io;
+
+import java.awt.event.KeyListener;
+
+
+/** 
+* 类: IKeyControlStack <br />
+* 叙述:键盘监听器栈<br />
+* @author 三向板砖 thrblock badteeth@qq.com
+* 注释日期 2016年3月20日 下午8:23:53 
+*/
+public interface IKeyControlStack {
+	public void pushKeyListener(KeyListener keyListener);
+	public KeyListener popKeyListener();
+	public boolean isKeyDown(int code);
+}
