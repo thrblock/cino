cd2f6a58a9057f04576e4e96204584819a10f951
zepu.li <zepu.li@corp.elong.com>
2016-11-25 19:18:20 +0800

    add: 着色器

---
+++ src/main/java/com/thrblock/cino/shader/GLProgramContainer.java
@@ -0,0 +1,16 @@
+package com.thrblock.cino.shader;
+
+import org.springframework.stereotype.Component;
+
+import com.jogamp.opengl.GL2;
+
+/**
+ * OpenGL可编程组件，程序容器
+ * @author zepu.li
+ */
+@Component
+public class GLProgramContainer {
+	public void parseShaderProgram(GL2 gl2) {
+		
+	}
+}
