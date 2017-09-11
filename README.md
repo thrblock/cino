c94a2a462a1d709de0393951500a07f3de2f0ce3
李泽溥 <zepu.li@corp.elong.com>
2017-09-11 15:43:31 +0800

    add: commons font name

--- README.md
+++ README.md
@@ -3,3 +3,4 @@ TODO:
 1.GLCharArea - > GLTextArea/GLTextLine
 [F]2.GLPostProcess - > GLFBOManager
 [F]3.GLLine -> theta redefine
+4.支持多种投影模式[大坑]
---
+++ src/main/java/com/thrblock/cino/gltexture/FontName.java
@@ -0,0 +1,40 @@
+package com.thrblock.cino.gltexture;
+
+/**
+ * Font names comming from macos
+ * @author thrblock
+ *
+ */
+public class FontName {
+    public static final String ARIAL = "Arial";
+    public static final String ARIAL_BLACK = "Arial Black";
+    public static final String ARIAL_HEBREW = "Arial Hebrew";
+    public static final String ARIAL_HEBREW_SCHOLAR = "Arial Hebrew Scholar";
+    public static final String ARIAL_NARROW = "Arial Narrow";
+    public static final String ARIAL_ROUNDED_MT_BOLD = "Arial Rounded MT Bold";
+    public static final String ARIAL_UNICODE_MS = "Arial Unicode MS";
+    public static final String LUCIDA_BLACKLETTER = "Lucida Blackletter";
+    public static final String LUCIDA_BRIGHT = "Lucida Bright";
+    public static final String LUCIDA_CALLIGRAPHY = "Lucida Calligraphy";
+    public static final String LUCIDA_CONSOLE = "Lucida Console";
+    public static final String LUCIDA_FAX = "Lucida Fax";
+    public static final String LUCIDA_GRANDE = "Lucida Grande";
+    public static final String LUCIDA_HANDWRITING = "Lucida Handwriting";
+    public static final String LUCIDA_SANS = "Lucida Sans";
+    public static final String LUCIDA_SANS_TYPEWRITER = "Lucida Sans Typewriter";
+    public static final String LUCIDA_SANS_UNICODE = "Lucida Sans Unicode";
+    public static final String HELVETICA = "Helvetica";
+    public static final String HELVETICA_NEUE = "Helvetica Neue";
+    public static final String VERDANA = "Verdana";
+    public static final String TAHOMA = "Tahoma";
+    public static final String TREBUCHET_MS = "Trebuchet MS";
+    public static final String TIMES_NEW_ROMAN = "Times New Roman";
+    
+    public static final String HEITI_SC = "Heiti SC";
+    public static final String HEITI_TC = "Heiti TC";
+    public static final String SONGTI_SC = "Songti SC";
+    public static final String SONGTI_TC = "Songti TC";
+
+    private FontName() {
+    }
+}
