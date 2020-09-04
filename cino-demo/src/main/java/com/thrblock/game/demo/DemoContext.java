1519ffbf77e2ae2c79f1fdfb69f39587415346cf
lizepu001 <lizepu001@ke.com>
2020-09-04 13:18:18 +0800

    remove demo

--- cino-demo/src/main/java/com/thrblock/game/demo/DemoContext.java
+++
@@ -1,9 +0,0 @@
-package com.thrblock.game.demo;
-
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-@ComponentScan(value = { "com.thrblock.game", "com.thrblock.aria" })
-public class DemoContext {
-}
